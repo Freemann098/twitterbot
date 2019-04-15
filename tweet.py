e902e7bffa1f3588a8c1664ea58c3d70e496aaa6
Chase Smith <freemann098@gmail.com>
2019-04-14 23:51:48 -0700

    Add files via upload

---
+++ tweet.py
@@ -0,0 +1,35 @@
+from twython import Twython
+import random
+
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+twitter = Twython(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+messages = [
+        "Have a good day",
+        "You can do it",
+        "I love you",
+        "You smell wondeful",
+        "Even though you smell bad, you look great",
+        "Atleast you tried!",
+        "You really are a good person, beleive me",
+        
+]
+
+def tweet():
+    message = random.choice(messages)
+    twitter.update_status(status=message)
+    print("Tweeted: {}".format(message))
+    t.start()
+
+t = Timer(3600, tweet)
+t.start()
