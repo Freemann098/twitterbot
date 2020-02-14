c61f350e93ab1333abd2791c72cc512b9a8cd479
Chase Smith <freemann098@gmail.com>
2020-02-14 16:54:02 -0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-consumer_key        = 'BTtmTSNq3xOvcTWmJZCceQYp2'
-consumer_secret     = 'fnw9l9B7JwRssWKIrERRMvWWGDIxVOR1299y571qM4eKHEzVPu'
-access_token        = '1117635539170025472-butSQNnVqLhFFI17Q5b6IWVjoiSBfU'
-access_token_secret = 'dGIVbG01DtGW7htKxApz25wJmVRbwJUbwNMqR0mvGAqdn'
+consumer_key        = ''
+consumer_secret     = ''
+access_token        = ''
+access_token_secret = ''
